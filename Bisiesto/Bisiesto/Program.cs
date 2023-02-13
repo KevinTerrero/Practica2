ebb00d20bfde36141fc218925cf5b1b3b9402787
KevinTerrero <94633933+KevinTerrero@users.noreply.github.com>
2023-02-13 16:05:40 -0400

    Add files via upload

---
+++ Bisiesto/Bisiesto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bisiesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduzca un año");
+
+            int año = int.Parse(Console.ReadLine());
+
+            if (año % 4 == 0 && año % 100 != 0)
+            {
+                Console.WriteLine("El año " + año + " es un año bisiesto");
+
+            }
+            else if (año % 4 == 0 && año % 100 != 0 && año % 400 == 0)
+            {
+                Console.WriteLine("El año " + año + " es un año bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año " + año + " NO es un año bisiesto");
+            }
+
+
+        Console.ReadKey();
+        }
+    }
+}
---
+++ Lotería/Lotería/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotería
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random rnd = new Random();
+
+            Console.WriteLine("Los tres numeros ganadores son:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(rnd.Next(0, 99));
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
